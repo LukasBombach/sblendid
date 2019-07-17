68d877e372ce5de3e673bbec03dff0541d0b3c8f
Lukas Bombach <lukas.bombach@googlemail.com>
2019-07-17 15:41:38 +0200

    improved development setup

--- packages/sblendid/development.ts
+++ packages/sblendid/development.ts
@@ -2,14 +2,32 @@ import Sblendid from "./src";
 
 (async () => {
   const sblendid = new Sblendid();
+  let i = 0;
 
   console.log("Powering on...");
   await sblendid.powerOn();
   console.log("Powered on!");
 
-  console.log("Startings to scan...");
-  sblendid.startScanning(peripheral => {
-    console.log("Found Peripheral");
-    console.log(peripheral);
+  console.log("Starting to scan...");
+  sblendid.startScanning(async peripheral => {
+    if (
+      !peripheral.advertisement ||
+      !peripheral.advertisement.localName ||
+      !peripheral.advertisement.serviceUuids
+    ) {
+      process.stdout.write(".");
+    } else {
+      console.log("");
+      const { uuid, rssi, advertisement } = peripheral;
+      const { localName, serviceUuids } = advertisement;
+      console.log(localName, `(${rssi})`, `(${uuid})`);
+      console.log(serviceUuids);
+      if (++i >= 3) {
+        console.log(`Stopping scanning after ${i} peripherals`);
+        await sblendid.stopScanning();
+        console.log("Stopped");
+        process.exit();
+      }
+    }
   });
 })();
