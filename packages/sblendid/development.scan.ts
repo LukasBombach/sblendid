7c154c9fa0b8e05c876f27d56a3b1eed704814df
Lukas Bombach <lukas.bombach@googlemail.com>
2019-07-19 11:36:45 +0200

    stuff

---
+++ packages/sblendid/development.scan.ts
@@ -0,0 +1,29 @@
+import chalk from "chalk";
+import Sblendid from "./src";
+
+(async () => {
+  try {
+    const sblendid = new Sblendid();
+    await sblendid.powerOn();
+
+    sblendid.startScanning(async peripheral => {
+      const { uuid, address, addressType, connectable, advertisement, rssi } = peripheral;
+
+      if (!advertisement) {
+        return console.log(chalk.dim(uuid), connectable, connectable);
+      }
+      if (!advertisement.manufacturerData) {
+        return console.log(chalk.dim(uuid), connectable, advertisement);
+      }
+      if (advertisement.manufacturerData) {
+        return console.log(
+          chalk.dim(uuid),
+          connectable,
+          chalk.blue(advertisement.manufacturerData.toString("hex"))
+        );
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+})();
