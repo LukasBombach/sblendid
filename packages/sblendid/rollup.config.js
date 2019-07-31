e0c29ef33721eaa3d91952c4e03d02803deb371f
Lukas Bombach <lukas.bombach@googlemail.com>
2019-07-31 20:41:02 +0200

    cleaned up build script

--- packages/sblendid/rollup.config.js
+++ packages/sblendid/rollup.config.js
@@ -2,25 +2,24 @@ import typescript from "typescript";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import typescriptPlugin from "rollup-plugin-typescript2";
+import autoExternal from "rollup-plugin-auto-external";
 import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
 export default {
   input: "src/index.ts",
-  external: ["events"],
   output: [
     {
       file: pkg.main,
-      format: "cjs",
-      preferBuiltins: true
+      format: "cjs"
     },
     {
       file: pkg.module,
-      format: "es",
-      preferBuiltins: true
+      format: "es"
     }
   ],
   plugins: [
+    autoExternal(),
     resolve({ preferBuiltins: true }),
     commonjs(),
     typescriptPlugin({ typescript }),
