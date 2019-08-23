a0a69f7354cc1b2d824d1325648ac5e57ce551db
Lukas Bombach <lukas.bombach@googlemail.com>
2019-08-23 12:17:28 +0200

    build setup

---
+++ packages/adapter-node/rollup.config.js
@@ -0,0 +1,28 @@
+import typescript from "typescript";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import typescriptPlugin from "rollup-plugin-typescript2";
+import autoExternal from "rollup-plugin-auto-external";
+import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs"
+    },
+    {
+      file: pkg.module,
+      format: "es"
+    }
+  ],
+  plugins: [
+    autoExternal(),
+    resolve({ preferBuiltins: true }),
+    commonjs(),
+    typescriptPlugin({ typescript }),
+    terser()
+  ]
+};
