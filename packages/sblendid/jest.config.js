0ed0d31ba8d601f0b31a12ff044f14f5f8acfc07
Lukas Bombach <lukas.bombach@googlemail.com>
2019-09-03 16:03:27 +0200

    jest config

--- packages/sblendid/jest.config.js
+++ packages/sblendid/jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   roots: ["<rootDir>/src"],
+  verbose: true,
+  notify: true,
+  notifyMode: "always",
+  collectCoverage: true,
   transform: {
     "^.+\\.ts$": "ts-jest"
   }
