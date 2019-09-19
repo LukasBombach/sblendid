10ec5270fcf3a2a1ba813d41fdd0beeae85f2f22
Lukas Bombach <lukas.bombach@googlemail.com>
2019-09-19 12:11:05 +0200

    Working on the Readme

--- packages/sblendid/examples/echo.ts
+++ packages/sblendid/examples/echo.ts
@@ -5,12 +5,10 @@ import Sblendid from "../src";
   const echoService = await peripheral.getService("ec00");
   let count = 0;
 
-  echoService!.on("ec0e", async data => {
-    console.log("Got Echo", data.toString());
-  });
+  echoService!.on("ec0e", data => console.log("Got Echo", data.toString()));
 
   setInterval(async () => {
-    const message = new Buffer(`Hello, BLE ${++count}`, "utf8");
+    const message = new Buffer(`Hello ${count++}`, "utf8");
     await echoService!.write("ec0e", message);
   }, 2500);
 })();
