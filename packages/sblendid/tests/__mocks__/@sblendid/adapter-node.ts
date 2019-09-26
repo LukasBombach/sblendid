ccb90cac8d13c4fd2031633fb8d74c5e95525f79
Lukas Bombach <lukas.bombach@googlemail.com>
2019-09-26 09:54:47 +0200

    mock adapter

--- packages/sblendid/tests/__mocks__/@sblendid/adapter-node.ts
+++ packages/sblendid/tests/__mocks__/@sblendid/adapter-node.ts
@@ -1,4 +1,8 @@
-import Adapter, { Params, Advertisement } from "@sblendid/adapter-node";
+import Adapter, {
+  Params,
+  Advertisement,
+  CharacteristicData
+} from "@sblendid/adapter-node";
 
 export const isMock = Boolean(process.env.USE_BLE);
 export class AdapterMock {}
@@ -11,37 +15,31 @@ export const discoverParams: Params<"discover"> = [
   advertisement,
   1
 ];
+export const suuids = ["suuid-1", "suuid-2", "suuid-3"];
+export const rssi = 1;
+export const characteristicDatas: CharacteristicData[] = [
+  { uuid: "cuuid-1", properties: { read: true, write: false, notify: false } },
+  { uuid: "cuuid-2", properties: { read: false, write: true, notify: false } },
+  { uuid: "cuuid-3", properties: { read: false, write: false, notify: true } }
+];
+export const readBuffer = Buffer.from("read", "utf8");
+export const notify = true;
 
 Object.assign(AdapterMock.prototype, {
-  powerOn: jest.fn().mockResolvedValue(undefined),
-  startScanning: jest.fn(),
-  stopScanning: jest.fn(),
-  find: jest.fn().mockResolvedValue(discoverParams),
-  connect: jest.fn().mockResolvedValue(undefined),
-  disconnect: jest.fn().mockResolvedValue(undefined),
-  getServices: jest.fn(),
-  getRssi: jest.fn(),
-  on: jest.fn(),
-  off: jest.fn(),
-  getCharacteristics: jest.fn(),
-  read: jest.fn(),
-  write: jest.fn(),
-  notify: jest.fn()
+  powerOn: jest.fn().mockResolvedValue(undefined), // powerOn(): Promise<void>;
+  startScanning: jest.fn(), // startScanning(): void;
+  stopScanning: jest.fn(), // stopScanning(): void;
+  find: jest.fn().mockResolvedValue(discoverParams), // find(condition: FindCondition): Promise<Params<"discover">>;
+  connect: jest.fn().mockResolvedValue(undefined), // connect(pUUID: PUUID): Promise<void>;
+  disconnect: jest.fn().mockResolvedValue(undefined), // disconnect(pUUID: PUUID): Promise<void>;
+  getServices: jest.fn().mockResolvedValue(suuids), // getServices(pUUID: PUUID): Promise<SUUID[]>;
+  getRssi: jest.fn().mockResolvedValue(rssi), // getRssi(pUUID: PUUID): Promise<number>;
+  on: jest.fn(), // on<E extends Event>(event: E, listener: Listener<E>): void;
+  off: jest.fn(), // off<E extends Event>(event: E, listener: Listener<E>): void;
+  getCharacteristics: jest.fn().mockResolvedValue(characteristicDatas), // getCharacteristics(pUUID: PUUID, sUUID: SUUID): Promise<CharacteristicData[]>;
+  read: jest.fn().mockResolvedValue(readBuffer), // read(pUUID: PUUID, sUUID: SUUID, cUUID: CUUID): Promise<Buffer>;
+  write: jest.fn().mockResolvedValue(undefined), // write(pUUID: PUUID, sUUID: SUUID, cUUID: CUUID, value: Buffer, withoutResponse?: boolean): Promise<void>;
+  notify: jest.fn().mockResolvedValue(notify) // notify(pUUID: PUUID, sUUID: SUUID, cUUID: CUUID, notify: boolean): Promise<boolean>;
 });
-
-// powerOn(): Promise<void>;
-// startScanning(): void;
-// stopScanning(): void;
-// find(condition: FindCondition): Promise<Params<"discover">>;
-// connect(pUUID: PUUID): Promise<void>;
-// disconnect(pUUID: PUUID): Promise<void>;
-// getServices(pUUID: PUUID): Promise<SUUID[]>;
-// getRssi(pUUID: PUUID): Promise<number>;
-// on<E extends Event>(event: E, listener: Listener<E>): void;
-// off<E extends Event>(event: E, listener: Listener<E>): void;
-// getCharacteristics(pUUID: PUUID, sUUID: SUUID): Promise<CharacteristicData[]>;
-// read(pUUID: PUUID, sUUID: SUUID, cUUID: CUUID): Promise<Buffer>;
-// write(pUUID: PUUID, sUUID: SUUID, cUUID: CUUID, value: Buffer, withoutResponse?: boolean): Promise<void>;
-// notify(pUUID: PUUID, sUUID: SUUID, cUUID: CUUID, notify: boolean): Promise<boolean>;
 
 export default isMock ? Adapter : AdapterMock;
