cf191606cc3e7a19eeec4c496293ff349d017813
Lukas Bombach <lukas.bombach@googlemail.com>
2019-09-27 17:09:46 +0200

    more cleanup

--- packages/sblendid/tests/sblendid.test.ts
+++ packages/sblendid/tests/sblendid.test.ts
@@ -5,13 +5,13 @@ import Sblendid, { Peripheral, Condition } from "../src";
 describe("Sblendid", () => {
   const name = "Find Me";
   const max = 5;
-  const findConditions: [string, Condition][] = [
+  const conditions: [string, Condition][] = [
     ["by name", name],
     ["using a callback", p => !!p.connectable],
     ["using an async callback", p => new Promise(res => res(!!p.connectable))]
   ];
 
-  it("can power on the adapter using its static method", async () => {
+  it("powers on the adapter using its static method", async () => {
     const spy = jest.spyOn(Adapter.prototype, "powerOn");
     const sblendid = await Sblendid.powerOn();
     expect(sblendid).toBeInstanceOf(Sblendid);
@@ -25,35 +25,32 @@ describe("Sblendid", () => {
     expect(inspect(spyReturn)).toBe("Promise { undefined }");
   });
 
-  it.each(findConditions)(
-    "can connect to a peripheral %s",
-    async (_, condition) => {
-      const spy = jest.spyOn(Adapter.prototype, "find");
-      const peripheral = await Sblendid.connect(condition);
-      const findCondition = spy.mock.calls[0][spy.mock.calls[0].length - 1];
-      const adv = { localName: "Find Me" };
-      const result = findCondition("uuid", "address", "public", true, adv, 1);
-      expect(peripheral).toBeInstanceOf(Peripheral);
-      expect(peripheral.isConnected()).toBe(true);
-      expect(spy).toHaveBeenCalledWith(expect.any(Function));
-      expect(result).toBe(true);
-      await peripheral.disconnect();
-    }
-  );
+  it.each(conditions)("connects to a peripheral %s", async (_, condition) => {
+    const spy = jest.spyOn(Adapter.prototype, "find");
+    const peripheral = await Sblendid.connect(condition);
+    const findCondition = spy.mock.calls[0][spy.mock.calls[0].length - 1];
+    const adv = { localName: "Find Me" };
+    const result = findCondition("uuid", "address", "public", true, adv, 1);
+    expect(peripheral).toBeInstanceOf(Peripheral);
+    expect(peripheral.isConnected()).toBe(true);
+    expect(spy).toHaveBeenCalledWith(expect.any(Function));
+    expect(result).toBe(true);
+    await peripheral.disconnect();
+  });
 
-  it.each(findConditions)("can find a peripheral %s", async (_, condition) => {
+  it.each(conditions)("finds a peripheral %s", async (_, condition) => {
     const sblendid = await Sblendid.powerOn();
     const peripheral = await sblendid.find(condition);
     expect(peripheral).toBeInstanceOf(Peripheral);
   });
 
-  it.skip("can start scanning without a callback", async () => {
+  it.skip("starts scanning without a callback", async () => {
     const sblendid = await Sblendid.powerOn();
     expect(() => sblendid.startScanning()).not.toThrow();
     sblendid.stopScanning();
   });
 
-  it.skip(`can scan for ${max} peripherals`, async () => {
+  it.skip(`scans for ${max} peripherals`, async () => {
     expect.assertions(1);
     let numFound = 0;
     const sblendid = await Sblendid.powerOn();
