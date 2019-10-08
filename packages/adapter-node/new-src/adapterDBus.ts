bceac95c5b47daaa5eabfe8afb831002ed1b9a26
Lukas Bombach <lukas.bombach@googlemail.com>
2019-10-08 12:36:55 +0200

    dbusadapter

--- packages/adapter-node/new-src/adapterDBus.ts
+++ packages/adapter-node/new-src/adapterDBus.ts
@@ -1,3 +1,46 @@
-import Adapter from "./adapter";
+import Adapter, { FindCondition, Characteristic } from "./adapter";
+import { Event, Params, Listener } from "./types/nobleAdapter";
 
-export default class DBusAdapter extends Adapter {}
+export default class DBusAdapter extends Adapter {
+  public init(): Promise<void> {}
+
+  public startScanning(): Promise<void> {}
+
+  public stopScanning(): Promise<void> {}
+
+  public find(condition: FindCondition): Promise<Params<"discover">> {}
+
+  public connect(pUUID: PUUID): Promise<void> {}
+
+  public disconnect(pUUID: PUUID): Promise<void> {}
+
+  public getRssi(pUUID: PUUID): Promise<number> {}
+
+  public getServices(pUUID: PUUID): Promise<SUUID[]> {}
+
+  public getCharacteristics(
+    pUUID: PUUID,
+    sUUID: SUUID
+  ): Promise<Characteristic[]> {}
+
+  public read(pUUID: PUUID, sUUID: SUUID, cUUID: CUUID): Promise<Buffer> {}
+
+  public write(
+    pUUID: PUUID,
+    sUUID: SUUID,
+    cUUID: CUUID,
+    value: Buffer,
+    withoutResponse: boolean
+  ): Promise<void> {}
+
+  public notify(
+    pUUID: PUUID,
+    sUUID: SUUID,
+    cUUID: CUUID,
+    notify: boolean
+  ): Promise<boolean> {}
+
+  public on<E extends Event>(event: E, listener: Listener<E>): Promise<void> {}
+
+  public off<E extends Event>(event: E, listener: Listener<E>): Promise<void> {}
+}
