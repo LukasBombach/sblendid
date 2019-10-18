a686cd36ec557bd0f31fd8e133f5086c4a3ea79c
Lukas Bombach <lukas.bombach@googlemail.com>
2019-10-18 19:39:42 +0200

    wip

--- packages/adapter-node/dev.ts
+++ packages/adapter-node/dev.ts
@@ -5,29 +5,13 @@ import Adapter from "./new-src";
     console.log("TS compiled, initializing adapter");
     const adapter = new Adapter();
     await adapter.init();
-    /* await adapter.on("discover", (...args) => console.log(...args));
-    await adapter.startScanning();
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    await adapter.stopScanning(); */
 
     const peripheral = await adapter.find(() => true);
     const [pUUID] = peripheral;
+
     console.log(peripheral);
 
     await adapter.connect(pUUID);
-
-    /* const peripheral = await adapter.find((...peripheral) => {
-      console.log(peripheral);
-      return ++connectables >= 5;
-    }); */
-
-    /* const peripheral = await adapter.find((...peripheral) => {
-      console.log(peripheral);
-      const [, , , , { localName }] = peripheral;
-      return localName === "ROIDMI Cleaner F1";
-    });
-
-    console.log("\n", "found", "\n", peripheral); */
 
     process.exit();
   } catch (error) {
