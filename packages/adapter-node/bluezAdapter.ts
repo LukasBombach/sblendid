c3e3753395893c4c6da89aca4ad6256e1e7fd540
Lukas Bombach <lukas.bombach@googlemail.com>
2019-11-01 11:55:51 +0100

    connecting and disconnecting work without errors

--- packages/adapter-node/bluezAdapter.ts
+++ packages/adapter-node/bluezAdapter.ts
@@ -5,15 +5,24 @@ import Bluez from "./src/adapterBluez";
     const bluez = new Bluez();
     await bluez.init();
 
-    const namedPeripheral = await bluez.find((...peripheral) => {
+    const roidmi = await bluez.find((...peripheral) => {
       const [, , , , { localName }] = peripheral;
       process.stdout.write(".");
-      return Boolean(localName);
+      return !!localName && /roidmi/i.test(localName);
     });
 
     console.log("\n", "Found this:");
-    console.log(namedPeripheral);
+    console.log(roidmi);
 
+    const [uuid] = roidmi;
+
+    console.log("connecting");
+    await bluez.connect(uuid);
+
+    console.log("disconnecting");
+    await bluez.disconnect(uuid);
+
+    console.log("done");
     process.exit();
   } catch (error) {
     console.error(error);
