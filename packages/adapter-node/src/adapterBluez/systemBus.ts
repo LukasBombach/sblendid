cac079e1ac8457ddef87223c92fa71548de32ccf
Lukas Bombach <lukas.bombach@googlemail.com>
2019-11-06 09:44:09 +0100

    changed fetchInterface implementation

--- packages/adapter-node/src/adapterBluez/systemBus.ts
+++ packages/adapter-node/src/adapterBluez/systemBus.ts
@@ -18,13 +18,26 @@ type FixedDBusInterface = DBusInterface & DBusInterfaceObject;
 type PromiseFn = (...args: any[]) => Promise<any>;
 type MethodTuple = [string, PromiseFn];
 
+type FetchInterface = (
+  service: string,
+  path: string,
+  name: string
+) => Promise<FixedDBusInterface>;
+
 export default class SystemBus {
   private static bus = DBus.getBus("system");
+  private fetchInterface: FetchInterface;
+
+  constructor() {
+    const getInterface = SystemBus.bus.getInterface.bind(SystemBus.bus);
+    this.fetchInterface = promisify(getInterface) as FetchInterface;
+  }
 
   public async getInterface<M extends Record<string, PromiseFn>>(
     params: InterfaceParams
   ): Promise<M> {
-    const iface = await this.fetchInterface(params);
+    const { service, path, name } = params;
+    const iface = await this.fetchInterface(service, path, name);
     const methods = this.getMethods(iface);
     return methods.reduce<M>((api, [n, m]) => ({ ...api, [n]: m }), {} as M);
   }
@@ -32,16 +45,5 @@ export default class SystemBus {
   private getMethods(iface: FixedDBusInterface): MethodTuple[] {
     const methodNames = Object.keys(iface.object.method);
     return methodNames.map(n => [n, promisify(iface.n.bind(iface))]);
-  }
-
-  private fetchInterface(params: InterfaceParams): Promise<FixedDBusInterface> {
-    return new Promise((resolve, reject) => {
-      const { service, path, name } = params;
-      SystemBus.bus.getInterface(service, path, name, (err, iface) => {
-        return err
-          ? reject(new Error(`${err.message}: ${service} ${path} ${name}`))
-          : resolve(iface as FixedDBusInterface);
-      });
-    });
   }
 }
