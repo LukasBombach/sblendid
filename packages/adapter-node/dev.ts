ad063a6c3aefbd4b8b59d0b567609337f90ad490
Lukas Bombach <lukas.bombach@googlemail.com>
2019-11-06 19:00:16 +0100

    dev script works great with new bluez api

--- packages/adapter-node/dev.ts
+++ packages/adapter-node/dev.ts
@@ -1,49 +1,17 @@
-import SystemBus from "./src/linux/systemBus";
-
-export interface Interfaces {
-  "org.bluez.Device1"?: boolean;
-  "org.bluez.GattService1"?: boolean;
-  "org.bluez.GattCharacteristic1"?: boolean;
-}
-
-interface AdapterApi {
-  methods: {
-    StartDiscovery: () => Promise<void>;
-    StopDiscovery: () => Promise<void>;
-  };
-}
-
-interface ObjectManagerApi {
-  events: {
-    InterfacesAdded: (path: string, interfaces: Interfaces) => void;
-  };
-}
+import Bluez from "./src/linux/bluez";
 
 (async () => {
   try {
-    // const bluezDBus = new BluezDBus();
-    // const adapter = await bluezDBus.getAdapter();
-    // const objectManager = await bluezDBus.getObjectManager();
-
-    const systemBus = new SystemBus();
-
-    const adapter = await systemBus.getInterface<AdapterApi>(
-      "org.bluez",
-      "/org/bluez/hci0",
-      "org.bluez.Adapter1"
-    );
-    const objectManager = await systemBus.getInterface<ObjectManagerApi>(
-      "org.bluez",
-      "/",
-      "org.freedesktop.DBus.ObjectManager"
-    );
+    const bluez = new Bluez();
+    const adapter = await bluez.getAdapter();
+    const objectManager = await bluez.getObjectManager();
 
     objectManager.on("InterfacesAdded", (path, iface) => {
       console.log(path, iface);
     });
 
     await adapter.StartDiscovery();
-    await new Promise(res => setTimeout(res, 5000));
+    await new Promise(res => setTimeout(res, 2000));
     await adapter.StopDiscovery();
 
     process.exit();
