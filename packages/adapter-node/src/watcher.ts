549d506104a6d45f4f3ee1eef405c16afffd41c6
Lukas Bombach <lukas.bombach@googlemail.com>
2019-11-07 16:15:59 +0100

    watcher implementatiopn is ther but i don't knw

--- packages/adapter-node/src/watcher.ts
+++ packages/adapter-node/src/watcher.ts
@@ -4,13 +4,37 @@ interface EventEmitter {
 }
 
 type Condition = (...args: any[]) => Promise<boolean> | boolean;
+type Resolver<T> = (value?: T | PromiseLike<T> | undefined) => void;
 
-export default class Watcher<T> {
-  constructor(
-    eventEmitter: EventEmitter,
-    event: string,
-    condition: Condition
-  ) {}
+export default class Watcher<T extends any[]> {
+  private eventEmitter: EventEmitter;
+  private event: string;
+  private listener: (...args: T) => void;
+  private promise: Promise<T>;
+  private resolve!: Resolver<T>;
 
-  public resolved(): Promise<T> {}
+  constructor(eventEmitter: EventEmitter, event: string, condition: Condition) {
+    this.eventEmitter = eventEmitter;
+    this.event = event;
+    this.listener = (...args: T) => condition(...args) && this.resolve(args);
+    this.promise = new Promise<T>(this.setResolver).then(this.stopListening);
+    this.startListening();
+  }
+
+  public resolved(): Promise<T> {
+    return this.promise;
+  }
+
+  private startListening(): void {
+    this.eventEmitter.on(this.event, this.listener);
+  }
+
+  private stopListening(args: T): T {
+    this.eventEmitter.off(this.event, this.listener);
+    return args;
+  }
+
+  private setResolver(resolve: Resolver<T>): void {
+    this.resolve = resolve;
+  }
 }
