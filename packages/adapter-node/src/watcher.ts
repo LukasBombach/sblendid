d91f62011f0854f4f826ee26997e209c2b2f52d5
Lukas Bombach <lukas.bombach@googlemail.com>
2019-11-17 21:19:15 +0100

    moved watcher types to own file

--- packages/adapter-node/src/watcher.ts
+++ packages/adapter-node/src/watcher.ts
@@ -1,23 +1,5 @@
+import { Emitter, Event, Value, Condition } from "../types/watcher";
 import Queue from "./queue";
-
-export type Emitter<E, L extends Listener> = {
-  on: (event: E, listener: L) => any;
-  off: (event: E, listener: L) => any;
-};
-
-export type Listener = (...args: any[]) => any;
-
-export type Event<A extends Emitter<any, any>> = A extends Emitter<infer T, any>
-  ? T
-  : never;
-
-export type Value<A extends Emitter<any, any>> = A extends Emitter<any, infer T>
-  ? Parameters<T>
-  : never;
-
-export type Condition<A extends Emitter<any, any>> = (
-  ...args: Value<A>
-) => Promise<boolean> | boolean;
 
 export default class Watcher<A extends Emitter<any, any>, E extends Event<A>> {
   private promise: Promise<Value<A>>;
