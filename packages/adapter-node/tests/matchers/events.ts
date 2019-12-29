fbfec1ebb2a92e04074c8f3e5fb669ab73879e0d
Lukas Bombach <lukas.bombach@googlemail.com>
2019-12-29 13:27:08 +0100

    made tests work for the adapter

--- packages/adapter-node/tests/matchers/events.ts
+++ packages/adapter-node/tests/matchers/events.ts
@@ -1,14 +1,6 @@
 import util from "util";
 import { events, Event } from "../utils/types";
 
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toBeEvent(argument: Event): R;
-    }
-  }
-}
-
 const i = (data: any) => util.inspect(data, { depth: 10 });
 
 expect.extend({
