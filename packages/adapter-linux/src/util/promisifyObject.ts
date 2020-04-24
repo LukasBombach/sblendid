f41a1da8ba3ae88d6953c5979ff944d3e4f72180
Lukas Bombach <lukas.bombach@googlemail.com>
2020-04-24 10:53:32 +0200

    finished up promisifyObject

--- packages/adapter-linux/src/util/promisifyObject.ts
+++ packages/adapter-linux/src/util/promisifyObject.ts
@@ -12,10 +12,6 @@ type Promisified<T> = {
   [K in keyof T]: T[K] extends AnyFunction ? PromisifiedFunction<T[K]> : never;
 };
 
-/* type PromisifiedEntries<T> = T extends Obj
-  ? [keyof T, PromisifiedFunction<T[keyof T]>][]
-  : never; */
-
 type Obj = Record<string, AnyFunction>;
 
 type Promisify = <N extends string, F extends AnyFunction>([N, F]) => [
@@ -26,12 +22,8 @@ type Promisify = <N extends string, F extends AnyFunction>([N, F]) => [
 export default function promisifyObject<T extends Obj>(obj: T): Promisified<T> {
   const promisify: Promisify = ([name, fn]) => [name, util.promisify(fn)];
   const entries = Object.entries(obj).map(promisify);
-  return fromEntries(entries);
-}
-
-function fromEntries<T>(entries: [keyof T, T[keyof T]][]): T {
   return entries.reduce(
     (acc, [key, value]) => ({ ...acc, [key]: value }),
-    <T>{}
+    {} as Promisified<T>
   );
 }
