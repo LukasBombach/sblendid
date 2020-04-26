5e8688662b47363712e99998496076fce75b964f
Lukas Bombach <lukas.bombach@googlemail.com>
2020-04-26 17:16:14 +0200

    wrote working promisify

--- packages/adapter-linux/promisify.d.ts
+++ packages/adapter-linux/promisify.d.ts
@@ -1,62 +1,35 @@
-type Callback<T> = (err: Error | null, reply: T) => void;
-// prettier-ignore
-type PromisifyOne<T extends any[]> =
-    T extends [Callback<infer U>?] ? () => Promise<U> :
-    T extends [infer T1, Callback<infer P>?] ? (arg1: T1) => Promise<P> :
-    T extends [infer T1, infer T2, Callback<infer U>?] ? (arg1: T1, arg2: T2) => Promise<U> :
-    T extends [infer T1, infer T2, infer T3, Callback<infer U>?]? (arg1: T1, arg2: T2, arg3: T3) => Promise<U> :
-    T extends [infer T1, infer T2, infer T3, infer T4, Callback<infer U>?] ? (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => Promise<U> :
-    never;
+type Promisify<T> = T extends (callback: (err: Error | null) => void) => void
+  ? () => Promise<void>
+  : T extends (
+      callback: (err: Error | null, result: infer TResult) => void
+    ) => void
+  ? () => Promise<TResult>
+  : T extends (
+      arg1: infer T1,
+      callback: (err: Error | null, result: infer TResult) => void
+    ) => void
+  ? (arg1: T1) => Promise<TResult>
+  : T extends (
+      arg1: infer T1,
+      arg2: infer T2,
+      callback: (err: Error | null, result: infer TResult) => void
+    ) => void
+  ? (arg1: T1, arg2: T2) => Promise<TResult>
+  : T extends (
+      arg1: infer T1,
+      arg2: infer T2,
+      arg3: infer T3,
+      callback: (err: Error | null, result: infer TResult) => void
+    ) => void
+  ? (arg1: T1, arg2: T2, arg3: T3) => Promise<TResult>
+  : T extends (
+      arg1: infer T1,
+      arg2: infer T2,
+      arg3: infer T3,
+      arg4: infer T4,
+      callback: (err: Error | null, result: infer TResult) => void
+    ) => void
+  ? (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => Promise<TResult>
+  : never;
 
-// prettier-ignore
-type GetOverloadArgs<T> = 
-    T extends {
-        (...o: infer U) : void,
-        (...o: infer U2) : void,
-        (...o: infer U3) : void,
-        (...o: infer U4) : void,
-        (...o: infer U5) : void,
-        (...o: infer U6) : void,
-        (...o: infer U7) : void
-    } ? U | U2 | U3 | U4 | U5 | U6 | U7:
-    T extends {
-        (...o: infer U) : void,
-        (...o: infer U2) : void,
-        (...o: infer U3) : void,
-        (...o: infer U4) : void,
-        (...o: infer U5) : void,
-        (...o: infer U6) : void,
-    } ? U | U2 | U3 | U4 | U5 | U6:
-    T extends {
-        (...o: infer U) : void,
-        (...o: infer U2) : void,
-        (...o: infer U3) : void,
-        (...o: infer U4) : void,
-        (...o: infer U5) : void,
-    } ? U | U2 | U3 | U4 | U5:
-    T extends {
-        (...o: infer U) : void,
-        (...o: infer U2) : void,
-        (...o: infer U3) : void,
-        (...o: infer U4) : void,
-    } ? U | U2 | U3 | U4 :
-    T extends {
-        (...o: infer U) : void,
-        (...o: infer U2) : void,
-        (...o: infer U3) : void,
-    } ? U | U2 | U3 :
-    T extends {
-        (...o: infer U) : void,
-        (...o: infer U2) : void,
-    } ? U | U2 :
-    T extends {
-        (...o: infer U) : void,
-    } ? U :
-    never;
-
-// prettier-ignore
-type UnionToIntersection<U> = (U extends any ? (k: U)=>void : never) extends ((k: infer I)=>void) ? I : never
-
-type Promisify<T> = UnionToIntersection<PromisifyOne<GetOverloadArgs<T>>>;
-
-type PromisifyAll<T extends {}> = { [J in keyof T]: Promisify<T[J]> };
+type PromisifyAll<T extends {}> = { [K in keyof T]: Promisify<T[K]> };
